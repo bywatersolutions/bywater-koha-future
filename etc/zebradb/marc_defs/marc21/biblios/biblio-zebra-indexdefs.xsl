bywatersolutions/bywater-koha

PACIFICA - Add custom search indexes
PACIFICA - Add Methodology Searching

PACIFICA - Custom Indexes

Adds ccl.properties, bib1.att and record.abs entries, as well as hardcoded (non-JQuery) searches in OPAC advsearch

Conflicts:
	etc/zebradb/ccl.properties

PACIFICA - Custom indexes Fixes

new indexes were showing up in 'fewer options' mode only in template

PacificaProgram and ThesisAdvisor were not connected to w and p registers

Typo in ccl.properties

PACIFICA - Add zebra indexes LocalCorporateName, LocalPersonalName

LocalCorporateName (pointing to 791$b)
LocalPersonalName (pointing to 796$a and 796$e).

Conflicts:
	etc/zebradb/marc_defs/marc21/biblios/biblio-koha-indexdefs.xml

PACIFICA - BWS Custom: rt 31109 Add zebra index Theme

Add zebra index 'Theme' for 690 subfield a

etc/zebradb/marc_defs/marc21/biblios/biblio-zebra-indexdefs.xsl
@@ -749,7 +749,7 @@ definition file (probably something like {biblio,authority}-koha-indexdefs.xml) 
   <xslo:template mode="index_subfields" match="marc:datafield[@tag='653']">
     <xslo:for-each select="marc:subfield">
       <xslo:if test="contains('a', @code)">
-        <z:index name="Index-term-uncontrolled:w Index-term-uncontrolled:p Subject:w Subject:p">
+        <z:index name="Index-term-uncontrolled:w Index-term-uncontrolled:p Subject:w Subject:p Theme:p Theme:p">
           <xslo:value-of select="."/>
         </z:index>
       </xslo:if>
@@ -838,6 +838,13 @@ definition file (probably something like {biblio,authority}-koha-indexdefs.xml) 
     </xslo:for-each>
   </xslo:template>
   <xslo:template mode="index_subfields" match="marc:datafield[@tag='690']">
+    <xslo:for-each select="marc:subfield">
+      <xslo:if test="contains('a', @code)">
+        <z:index name="Theme:w Theme:p">
+          <xslo:value-of select="."/>
+        </z:index>
+      </xslo:if>
+    </xslo:for-each>
     <xslo:for-each select="marc:subfield">
       <xslo:if test="contains('9', @code)">
         <z:index name="Koha-Auth-Number:w">
@@ -1252,6 +1259,31 @@ definition file (probably something like {biblio,authority}-koha-indexdefs.xml) 
     <xslo:for-each select="marc:subfield">
       <xslo:if test="contains('z', @code)">
         <z:index name="ISBN:w Identifier-standard:w">
+          <xslo:value-of select="."/>
+        </z:index>
+      </xslo:if>
+    </xslo:for-each>
+  </xslo:template>
+  <xslo:template mode="index_subfields" match="marc:datafield[@tag='791']">
+    <xslo:for-each select="marc:subfield">
+      <xslo:if test="contains('b', @code)">
+        <z:index name="LocalCorporateName:w LocalCorporateName:p">
+          <xslo:value-of select="."/>
+        </z:index>
+      </xslo:if>
+    </xslo:for-each>
+  </xslo:template>
+  <xslo:template mode="index_subfields" match="marc:datafield[@tag='796']">
+    <xslo:for-each select="marc:subfield">
+      <xslo:if test="contains('a', @code)">
+        <z:index name="LocalPersonalName:w LocalPersonalName:p">
+          <xslo:value-of select="."/>
+        </z:index>
+      </xslo:if>
+    </xslo:for-each>
+    <xslo:for-each select="marc:subfield">
+      <xslo:if test="contains('e', @code)">
+        <z:index name="LocalPersonalName:w LocalPersonalName:p">
           <xslo:value-of select="."/>
         </z:index>
       </xslo:if>

bywatersolutions/bywater-koha-future

etc/zebradb/marc_defs/marc21/biblios/biblio-zebra-indexdefs.xsl
@@ -772,7 +772,7 @@ definition file (probably something like {biblio,authority}-koha-indexdefs.xml) 
   <xslo:template mode="index_subfields" match="marc:datafield[@tag='653']">
     <xslo:for-each select="marc:subfield">
       <xslo:if test="contains('a', @code)">
-        <z:index name="Index-term-uncontrolled:w Index-term-uncontrolled:p Subject:w Subject:p">
+        <z:index name="Index-term-uncontrolled:w Index-term-uncontrolled:p Subject:w Subject:p Theme:w Theme:p">
           <xslo:value-of select="."/>
         </z:index>
       </xslo:if>
@@ -861,6 +861,13 @@ definition file (probably something like {biblio,authority}-koha-indexdefs.xml) 
     </xslo:for-each>
   </xslo:template>
   <xslo:template mode="index_subfields" match="marc:datafield[@tag='690']">
+    <xslo:for-each select="marc:subfield">
+      <xslo:if test="contains('a', @code)">
+        <z:index name="Theme:w Theme:p">
+          <xslo:value-of select="."/>
+        </z:index>
+      </xslo:if>
+    </xslo:for-each>
     <xslo:for-each select="marc:subfield">
       <xslo:if test="contains('9', @code)">
         <z:index name="Koha-Auth-Number:w">
@@ -1275,6 +1282,31 @@ definition file (probably something like {biblio,authority}-koha-indexdefs.xml) 
     <xslo:for-each select="marc:subfield">
       <xslo:if test="contains('z', @code)">
         <z:index name="ISBN:w Identifier-standard:w">
+          <xslo:value-of select="."/>
+        </z:index>
+      </xslo:if>
+    </xslo:for-each>
+  </xslo:template>
+  <xslo:template mode="index_subfields" match="marc:datafield[@tag='791']">
+    <xslo:for-each select="marc:subfield">
+      <xslo:if test="contains('b', @code)">
+        <z:index name="LocalCorporateName:w LocalCorporateName:p">
+          <xslo:value-of select="."/>
+        </z:index>
+      </xslo:if>
+    </xslo:for-each>
+  </xslo:template>
+  <xslo:template mode="index_subfields" match="marc:datafield[@tag='796']">
+    <xslo:for-each select="marc:subfield">
+      <xslo:if test="contains('a', @code)">
+        <z:index name="LocalPersonalName:w LocalPersonalName:p">
+          <xslo:value-of select="."/>
+        </z:index>
+      </xslo:if>
+    </xslo:for-each>
+    <xslo:for-each select="marc:subfield">
+      <xslo:if test="contains('e', @code)">
+        <z:index name="LocalPersonalName:w LocalPersonalName:p">
           <xslo:value-of select="."/>
         </z:index>
       </xslo:if>